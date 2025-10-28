f76a4dfa0f8e2579634fc9aa60b4140ad4f27e96
jo-oseeph <realsejjo2001@gmail.com>
2025-10-28 18:15:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-800">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind v4.1.16 Works ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
