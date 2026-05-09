b3b395813abc99b7935b69b09cfcf7603de3db30
jo-oseeph <realsejjo2001@gmail.com>
2026-05-09 16:29:01 +0300

    May 9

--- src/components/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp.jsx
@@ -35,7 +35,7 @@ const FloatingWhatsApp = () => {
 
       {/* Tooltip */}
       <div className="absolute bottom-full right-0 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
-        Chat with us
+        Quick Chat
         <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
       </div>
     </a>
